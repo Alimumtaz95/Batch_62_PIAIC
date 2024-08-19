0042d8dc0b6a6078853390360e98d76ff7229c13
Mumtaz Ali <mumtazalipintto@gmail.com>
2024-08-19 10:02:51 -0700

    First commit

---
+++ Variable.py
@@ -0,0 +1,9 @@
+Name:str="12"
+print(type(Name))
+
+Name:int=12
+print(Name)
+print(type(Name))
+#premitive and non permitive data types
+#explicit(Defining the type of a variable) and implicit (Without defining the type of a variable)
+# ipynb (interactive python note book) it can run a code in chunks 
